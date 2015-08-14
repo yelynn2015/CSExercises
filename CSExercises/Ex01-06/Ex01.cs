5454feb68cba272549992760549229ab9643f61b
yelynn2015 <1040677385@qq.com>
2015-08-14 13:50:01 +0800

    completed Exercise1

--- CSExercises/Ex01-06/Ex01.cs
+++ CSExercises/Ex01-06/Ex01.cs
@@ -10,7 +10,11 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            Console.Write("Please enter your name");
+            string name=Console.ReadLine();
+            Console.WriteLine("Good moring!"+name);
             
+
         }
     }
 }
