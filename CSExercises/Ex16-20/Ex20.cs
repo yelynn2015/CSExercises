893724591f0336bb608c53e5017425840bc9b4e5
yelynn2015 <1040677385@qq.com>
2015-08-17 15:51:19 +0800

    done it

--- CSExercises/Ex16-20/Ex20.cs
+++ CSExercises/Ex16-20/Ex20.cs
@@ -16,12 +16,12 @@ namespace CSExercises
             if (tv < 0 || dvd < 0 || mp3 < 0) Console.WriteLine("wrong input");
             else
             {
-                double charge_first = 900 * tv + 500 * dvd;
+                double charge_first = 900 * tv + 500 * dvd+700*mp3;
                 if (charge_first > 5000 && charge_first <= 10000)
-                    charge_first = 0.9 * charge_first;
+                    charge_first =(charge_first - 700 * mp3)*0.9; 
                 else if (charge_first > 10000)
-                    charge_first = 0.85 * charge_first;
-                double charge_whole = charge_first + mp3 * 700;
+                    charge_first =(charge_first-700*mp3)*0.85;
+                double charge_whole = charge_first + mp3*700 ;
                 Console.WriteLine("Total price for this order is $"+charge_whole);
             }
 
